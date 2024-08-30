cfbf4dcb1617de3456939974588fc878816df311
Bamos Guinley <guinleyb@gmail.com>
2024-08-30 19:50:20 +0200

    add orderId sertter & geetter to make attributs use

--- src/main/java/bj/highfiveuniveristy/ecommerce/models/OrderItemId.java
+++ src/main/java/bj/highfiveuniveristy/ecommerce/models/OrderItemId.java
@@ -9,4 +9,34 @@ import jakarta.persistence.Embeddable;
 public class OrderItemId implements Serializable {
     private Long orderId;
     private Long productId;
+    
+
+    /**
+     * @return Long return the orderId
+     */
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * @param orderId the orderId to set
+     */
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * @return Long return the productId
+     */
+    public Long getProductId() {
+        return productId;
+    }
+
+    /**
+     * @param productId the productId to set
+     */
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
 }
