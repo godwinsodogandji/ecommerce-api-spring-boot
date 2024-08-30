96d77b51a3d8ea2ce12b4fef39ada4dd992b71c8
Bamos Guinley <guinleyb@gmail.com>
2024-08-30 19:39:30 +0200

    update

--- src/main/java/bj/highfiveuniveristy/ecommerce/repositories/CategoryRepository.java
+++ src/main/java/bj/highfiveuniveristy/ecommerce/repositories/CategoryRepository.java
@@ -1,8 +1,9 @@
 package bj.highfiveuniveristy.ecommerce.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import bj.highfiveuniveristy.ecommerce.models.Category;
 
-
-public interface CategoryRepository  {
+public interface CategoryRepository  extends JpaRepository<Category,Long>{
 
 }
